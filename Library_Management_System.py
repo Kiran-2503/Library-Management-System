594aa407783ba6d0e28a760c395c98e6be9bbbb7
Kiran-2503 <kiranc2503@gmail.com>
2026-06-26 23:00:30 +0530

    Set up project structure and function skeleton

---
+++ Library_Management_System.py
@@ -0,0 +1,85 @@
+import csv
+from datetime import datetime
+
+# ==========================
+# Global Constants 
+# ==========================
+MANAGER_PIN = "2503"
+
+BOOKS_FILE = "books.csv"
+USERS_FILE = "users.csv"
+HISTORY_FILE = "history.csv"
+
+
+# ==========================
+# Manager Functions
+# ==========================
+
+def manager_login():
+    pass
+
+def manager_menu():
+    pass
+
+def add_user():
+    pass
+
+def remove_user():
+    pass
+
+def view_users():
+    pass
+
+def add_book():
+    pass
+
+def remove_book():
+    pass
+
+def view_books():
+    pass
+
+def search_book():
+    pass
+
+def view_transaction_history():
+    pass
+
+
+# ==========================
+# User Functions
+# ==========================
+
+def user_login():
+    pass
+
+def user_menu():
+    pass
+
+def view_available_books():
+    pass
+
+def issue_book():
+    pass
+
+def return_book():
+    pass
+
+def view_my_history():
+    pass
+
+
+# ==========================
+# Main Function
+# ==========================
+
+def main():
+    pass
+
+
+# ==========================
+# Program Entry Point
+# ==========================
+
+if __name__ == "__main__":
+    main()
